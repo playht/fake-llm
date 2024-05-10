adc7af6ef2655f5bb4126a0da84b6eda1fb4fd08
Antonio C. Jr. <acdcjunior@gmail.com>
2024-05-10 17:04:03 -0300

    chore: update curl demo baseUrl

--- src/curl-set-fake-server-on-agent.sh
+++ src/curl-set-fake-server-on-agent.sh
@@ -3,7 +3,7 @@
 AGENT_ID="my-agent-id-1234567890"
 API_KEY="myApiKey"
 USER_ID="myUserId"
-CUSTOM_LLM_URL="http://localhost:8080/"
+CUSTOM_LLM_URL="https://example1-exam-ple1-exam-ple1example1-ex-ample1example.riker.replit.dev/"
 CUSTOM_LLM_API_KEY="my-llm-api-key"
 
 curl -X PATCH "https://api.play.ai/api/v1/agents/${AGENT_ID}" \
